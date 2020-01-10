5730572d3c5bdc285a4fdd902ba39056a493ffcd
unknown <ahluwaliajyoti50@gmail.com>
2020-01-10 19:17:50 +0530

    10 jan 2020

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,48 +1,35 @@
-import React, {Component} from 'react';
+import React,{Component} from 'react';
 
 class Form extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            fullname: "Jyoti",
-            email: "geeksnotnerdsdotcom@gmail.com",
-            mobile: "9867715458",
-            message: "Hello!!!!"
+            fullname: "",
+            email: ""
         }
+        this.baseState = this.state;
     }
 
-    handlename = (event)=> {
-        console.log(event);
+    handlechange = (event) => {
+        this.setState ({[event.target.name]:event.target.value});
+    }
+
+    handlesubmit = (event) => {
+        let data = this.state;
+        console.log(data);
+        event.preventDefault();
+        this.setState(this.baseState);
     }
 
     render() {
         return (
-        
-                <form className="col-md-12">
-                    <div className="control">
-                        <label>Full Name: </label><br/>
-                        <input type="text" value={this.state.fullname} onChange={this.handlename} /><br/>
-                    </div>
-                    <div className="control">
-                        <label>Email: </label><br/>
-                        <input type="email" value={this.state.email} /><br/>
-                    </div>
-                    
-                    <div className="control">
-                        <label>Mobile: </label><br/>
-                        <input type="number" value={this.state.mobile} /><br/>
-                    </div>
-                    
-                    <div className="control">
-                        <label>Message: </label><br/>
-                        <textarea value={this.state.message}></textarea><br/>
-                    </div>
-
-                    <div className="control">
-                        <input type="submit" value="Submit" />
-                    </div>
-                    
-                </form>
+            <form onSubmit={this.handlesubmit}>
+                <label>Fullname</label>
+                <input type="text" name="fullname" value={this.state.fullname} onChange={this.handlechange} />
+                <label>Email</label>
+                <input type="email" name="email" value={this.state.email} onChange={this.handlechange} />
+                <button type="submit">Send</button>
+            </form>
         )
     }
 }
